6c13e33e8110217c34caac5f36463c654bc09bca
Elxiim <michelandrianarivoo@gmail.com>
2024-08-17 21:15:20 +0300

    remove unused on app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,4 @@ import { Cocktail } from './shared/interfaces/cocktail';
 })
 export class AppComponent {
   title = 'e-cocktails';
-
-  cocktail: Cocktail = {
-    name: 'mojito',
-    img: 'https://static.750g.com/images/640-420/b520523117d647dab6b842a36f4cc7f5/mojito-le-vrai.jpg',
-    description:
-      'Le mojito, prononcé [moˈxito] en espagnol, ou mojito, morito, ou mohito en français, est un cocktail traditionnel de la cuisine cubaine et de la culture de Cuba, à base de rhum, de soda, de citron vert, et de feuilles de menthe fraîche',
-  };
 }
