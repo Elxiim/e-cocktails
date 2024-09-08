8603194168016cdf984c6850d90ae61c528fd38d
Elxiim <michelandrianarivoo@gmail.com>
2024-09-08 12:32:43 +0300

    Shared Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 // component
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SelectedDirective } from './shared/directives/selected.directive';
 
 // routes
 import { APP_ROUTES } from './app.routes';
@@ -15,7 +14,7 @@ import { CocktailModule } from './features/cocktail/cocktail.module';
 import { PanierModule } from './features/panier/panier.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SelectedDirective],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
--- src/app/features/cocktail/cocktail.module.ts
+++ src/app/features/cocktail/cocktail.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 // Modules
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 // Components
 import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
 import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
 import { CocktailFormComponent } from './cocktail-container/cocktail-form/cocktail-form.component';
 import { CocktailListComponent } from './cocktail-container/cocktail-list/cocktail-list.component';
-import { RouterModule } from '@angular/router';
-import { FilterPipe } from 'src/app/shared/pipes/filter.pipe';
+import { COCKTAIL_ROUTES } from './cocktail.routes';
+import { SharedModule } from 'src/app/shared/modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -18,9 +18,13 @@ import { FilterPipe } from 'src/app/shared/pipes/filter.pipe';
     CocktailDetailsComponent,
     CocktailContainerComponent,
     CocktailFormComponent,
-    FilterPipe,
   ],
 
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule],
+  imports: [
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forChild(COCKTAIL_ROUTES),
+  ],
 })
 export class CocktailModule {}
